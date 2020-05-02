cfc362642e8899b69f4d4977ea9f267e54b8fd0c
guanpengchn <875499765@qq.com>
2020-05-02 14:31:43 +0800

    fix

--- detail-start/src/main/java/com/drawcode/detail/start/controller/TestController.java
+++ detail-start/src/main/java/com/drawcode/detail/start/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.drawcode.detail.start.controller;
 
 import com.drawcode.detail.service.Have;
+import com.drawcode.detail.service.RootAbility;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,41 +14,36 @@ public class TestController {
     @GetMapping("/")
     public String test() {
         List<Map<String, Class>> resList = new ArrayList<>();
-        // TitleAbility开始扫描
-        try {
-            Class rootClass = Class.forName("com.drawcode.detail.service.RootAbility");
+        Class rootClass = RootAbility.class;
 
-            Map<String, Class> rootMap = new HashMap<>();
-            rootMap.put(rootClass.getName(),rootClass);
-            resList.add(rootMap);
+        Map<String, Class> rootMap = new HashMap<>();
+        rootMap.put(rootClass.getName(), rootClass);
+        resList.add(rootMap);
 
-            Queue<Class> classQueue = new LinkedList<>();
-            classQueue.add(rootClass);
+        Queue<Class> classQueue = new LinkedList<>();
+        classQueue.add(rootClass);
 
-            while(!classQueue.isEmpty()) {
-                Class clazz = classQueue.poll();
-                Field[] fields = clazz.getDeclaredFields();
+        while (!classQueue.isEmpty()) {
+            Class clazz = classQueue.poll();
+            Field[] fields = clazz.getDeclaredFields();
 
-                Map<String, Class> clazzMap = new HashMap<>();
+            Map<String, Class> clazzMap = new HashMap<>();
 
-                for(Field field : fields) {
-                    Have have = field.getAnnotation(Have.class);
-                    if(have != null) {
-                        Class childClass = field.getType();
-                        classQueue.add(childClass);
-                        clazzMap.put(childClass.getName(),childClass);
-                    }
-                }
-
-                if(!clazzMap.isEmpty()) {
-                    resList.add(clazzMap);
+            for (Field field : fields) {
+                Have have = field.getAnnotation(Have.class);
+                if (have != null) {
+                    Class childClass = field.getType();
+                    classQueue.add(childClass);
+                    clazzMap.put(childClass.getName(), childClass);
                 }
             }
 
-            Collections.reverse(resList);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            if (!clazzMap.isEmpty()) {
+                resList.add(clazzMap);
+            }
         }
+
+        Collections.reverse(resList);
         return "1234";
     }
 }
