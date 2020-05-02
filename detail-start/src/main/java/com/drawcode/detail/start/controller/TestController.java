8f1a34f4c56cbbbf851fe976f1cebba7e690ddef
guanpengchn <875499765@qq.com>
2020-05-02 14:27:08 +0800

    添加遍历

--- detail-start/src/main/java/com/drawcode/detail/start/controller/TestController.java
+++ detail-start/src/main/java/com/drawcode/detail/start/controller/TestController.java
@@ -5,20 +5,46 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.reflect.Field;
+import java.util.*;
 
 @RestController
 public class TestController {
 
     @GetMapping("/")
     public String test() {
+        List<Map<String, Class>> resList = new ArrayList<>();
         // TitleAbility开始扫描
         try {
-            Class rootClass = Class.forName("com.drawcode.detail.service.TitleAbility");
-            Field[] fields = rootClass.getFields();
-            for(Field field : fields) {
-                Have have = field.getAnnotation(Have.class);
+            Class rootClass = Class.forName("com.drawcode.detail.service.RootAbility");
 
+            Map<String, Class> rootMap = new HashMap<>();
+            rootMap.put(rootClass.getName(),rootClass);
+            resList.add(rootMap);
+
+            Queue<Class> classQueue = new LinkedList<>();
+            classQueue.add(rootClass);
+
+            while(!classQueue.isEmpty()) {
+                Class clazz = classQueue.poll();
+                Field[] fields = clazz.getDeclaredFields();
+
+                Map<String, Class> clazzMap = new HashMap<>();
+
+                for(Field field : fields) {
+                    Have have = field.getAnnotation(Have.class);
+                    if(have != null) {
+                        Class childClass = field.getType();
+                        classQueue.add(childClass);
+                        clazzMap.put(childClass.getName(),childClass);
+                    }
+                }
+
+                if(!clazzMap.isEmpty()) {
+                    resList.add(clazzMap);
+                }
             }
+
+            Collections.reverse(resList);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
